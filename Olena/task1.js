d3eac4a7a3664721c8896151f1b9c5f38a8dcace
Elena <108807767+elenaberdnik@users.noreply.github.com>
2025-09-18 17:57:55 +0300

    hekko

--- Olena/task1.js
+++ Olena/task1.js
@@ -1,14 +1,18 @@
 let averageGrade = 85; 
 
-if (averageGrade >= 91 && averageGrade <=100){
- console.log("Відмінно"); }
-else if (averageGrade >= 81 && averageGrade <= 90) {
-     console.log("Дуже добре"); }
-else if (averageGrade >= 71 && averageGrade <=80 ) {
-        console.log("Добре"); }
-else if (averageGrade >= 60 && averageGrade <=70 ) {
-        console.log("Задовільно");
-} else {
-    console.log("Незадовільно");
+if (averageGrade < 60) {
+    console.log("Незадовільно")
 }
+else if (averageGrade <= 70 ) {
+        console.log("Задовільно");
+}
+else if (averageGrade <= 80 ) {
+        console.log("Добре"); }
+else if (averageGrade <= 90) {
+     console.log("Дуже добре"); }
 
+else if (averageGrade <=100){
+ console.log("Відмінно"); }
+ else {
+     console.log ("Перевір введені дані")
+ }
